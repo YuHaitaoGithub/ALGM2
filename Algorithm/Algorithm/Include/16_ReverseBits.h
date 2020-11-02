fd98281d7e2980ed73e7cd600e372cbb791e133b
MM-202010052142\Administrator <1424634450@qq.com>
2020-11-02 17:01:49 +0800

    写了第十六题的代码

---
+++ Algorithm/Algorithm/Include/16_ReverseBits.h
@@ -0,0 +1,3 @@
+#pragma once
+typedef unsigned int uint32_t;
+uint32_t ReverseBits(uint32_t n);
--- Algorithm/Algorithm/Str/16_ReverseBits.cpp
+++ Algorithm/Algorithm/Str/16_ReverseBits.cpp
@@ -4,6 +4,14 @@ typedef unsigned int uint32_t;
 
 uint32_t ReverseBits(uint32_t n)
 {
+	uint32_t x = 0;
 	uint32_t ret = 0;
+	int vaule = 0;
+	for (int i = 0; i < 32; i++)
+	{
+		if (n&(1 << i))vaule = 1;
+		else vaule = 0;
+		ret = ret + vaule * (1 << (31 - i));
+	}
 	return ret;
 }
