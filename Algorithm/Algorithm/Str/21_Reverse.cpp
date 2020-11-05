92a6aebeca3a27235ddc5a4e7426b5552b171d79
MM-202010052142\Administrator <1424634450@qq.com>
2020-11-05 13:45:22 +0800

    写了第二十一题的代码

---
+++ Algorithm/Algorithm/Str/21_Reverse.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "..\Include\21_Reverse.h"
+#include "stdio.h"
+#include "math.h"
+int Reverse(int x)
+{
+	char nums[13] = {};
+	char *max = "2147483647";
+	char *min = "2147483648";
+	int symbol = 1;
+	int len = sprintf(nums, "%d", x);
+	if (len < 1)return 0;
+	int j = 0;
+	if (nums[0] == '-')
+	{
+		if (len == 1)return 0;
+		if (len == 2)return x;
+		j = len;
+		if (len > 11)
+			return 0;
+		symbol = -1;
+	}
+	else
+	{
+		if (len == 1)return x;
+		j = len;
+		if (len > 10)
+			return 0;
+	}
+	char *p = symbol == 1 ? max : min;
+	if ((j == 10&&symbol == 1)||(j == 11&&symbol == -1))
+	{
+		for (int x = j -1; x >= 0; x--)
+		{
+			if (nums[x] - '0' > p[j-x-1] - '0')
+				return 0;
+			if (nums[x] - '0' < p[j - x - 1] - '0')
+				break;
+		}
+	}
+	int ret = 0;
+	if (symbol == -1)
+	{
+		j--;
+		for (int k = j; k > 0; k--)
+		{
+			ret = (int)pow(10.0, k - 1) * ((symbol)*(nums[k] - '0')) + ret;
+		}
+		return ret;
+	}
+	else {
+		for (int k = j; k > 0; k--)
+		{
+			ret = (int)pow(10.0, k - 1) * ((symbol)*(nums[k-1] - '0')) + ret;
+		}
+		return ret;
+	}
+
+}
